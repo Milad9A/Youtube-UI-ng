d7c64842495e8d48b2ca4574edf4add778c76b0b
Milad <millad.99.awad@gmail.com>
2022-05-08 17:53:19 +0300

    add video card widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_ng/data.dart';
 import 'package:youtube_ng/widgets/custom_sliver_app_bar.dart';
+import 'package:youtube_ng/widgets/video_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: CustomScrollView(
         slivers: [
-          CustomSliverAppBar(),
+          const CustomSliverAppBar(),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                final video = videos[index];
+                return VideoCard(video: video);
+              },
+              childCount: videos.length,
+            ),
+          ),
         ],
       ),
     );
